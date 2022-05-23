4fb6fde6a930f72018d9fd99616fe56fcf35b15f
oktaygulec <oktay.gulec@outlook.com>
2022-05-24 01:28:24 +0300

    first commit

---
+++ Gogus_Kanseri_Solution_Classification/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace FutbolOyna_Agac_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblHayattaKalmaDurumu = new System.Windows.Forms.Label();
+            this.txtNode = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(24, 347);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(281, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "HAYATTA KALMA DURUMU:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Location = new System.Drawing.Point(41, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(481, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "GÖĞÜS KANSERİ HAYATTA KALMA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Pozitif Aksiller Lenf Nodu Adeti";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnHesapla);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.txtNode);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(30, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(509, 217);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seçimler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Hastanın Operasyondaki Yaşı";
+            // 
+            // lblHayattaKalmaDurumu
+            // 
+            this.lblHayattaKalmaDurumu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHayattaKalmaDurumu.AutoSize = true;
+            this.lblHayattaKalmaDurumu.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHayattaKalmaDurumu.ForeColor = System.Drawing.Color.Green;
+            this.lblHayattaKalmaDurumu.Location = new System.Drawing.Point(311, 347);
+            this.lblHayattaKalmaDurumu.Name = "lblHayattaKalmaDurumu";
+            this.lblHayattaKalmaDurumu.Size = new System.Drawing.Size(23, 24);
+            this.lblHayattaKalmaDurumu.TabIndex = 11;
+            this.lblHayattaKalmaDurumu.Text = "?";
+            this.lblHayattaKalmaDurumu.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // txtNode
+            // 
+            this.txtNode.Location = new System.Drawing.Point(23, 80);
+            this.txtNode.Name = "txtNode";
+            this.txtNode.Size = new System.Drawing.Size(217, 20);
+            this.txtNode.TabIndex = 11;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(23, 164);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(217, 20);
+            this.txtAge.TabIndex = 12;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.Green;
+            this.btnHesapla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHesapla.ForeColor = System.Drawing.Color.White;
+            this.btnHesapla.Location = new System.Drawing.Point(291, 80);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(177, 79);
+            this.btnHesapla.TabIndex = 13;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(571, 409);
+            this.Controls.Add(this.lblHayattaKalmaDurumu);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Göğüs Kanseri Hayatta Kalma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHayattaKalmaDurumu;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtNode;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ Gogus_Kanseri_Solution_Classification/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FutbolOyna_Agac_Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblHayattaKalmaDurumu.Text = "?";
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int node = int.Parse(txtNode.Text);
+            int age = int.Parse(txtAge.Text);
+            if(node <= 4)
+                lblHayattaKalmaDurumu.Text = "5 Yıl veya Daha Fazla";
+            else
+            {
+                if(age > 42)
+                    lblHayattaKalmaDurumu.Text = "5 Yıldan Daha Az";
+                else
+                    lblHayattaKalmaDurumu.Text = "5 Yıl veya Daha Fazla";
+            }
+        }
+    }
+}
